9dd57bcc5a98b7bad42772313c5128999dde7987
FatimaALzahrani <12fatimah.15@gmail.com>
2023-03-19 15:40:57 +0300

    finsh from project

---
+++ index.js
@@ -0,0 +1,14 @@
+const myName = 'Ali'
+const value = 'hello world'
+const list = [1, 2, 3, 4]
+
+console.log(value)
+console.log(myName)
+console.log(list)
+
+/* الكود قبل
+ const my_name =    'Ali' ,  value = 'hello world';
+ let list = [1,2,3,4]
+
+ console.log(value);
+ */
